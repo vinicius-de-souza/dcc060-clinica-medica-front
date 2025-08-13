2bc94fad5ec5aac45ef48b3301f4952ec529394a
Maria Luiza Dornelas <mariadornelasc12@gmail.com>
2025-08-12 21:56:20 -0300

    Ajusta url da api

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Configuration
-//const API_BASE_URL = 'https://dcc060-clinica-medica.onrender.com/api';
-const API_BASE_URL = 'http://localhost:3000/api';
+const API_BASE_URL = 'https://dcc060-clinica-medica.onrender.com/api';
+//const API_BASE_URL = 'http://localhost:3000/api';
 
 
 // Global variables
